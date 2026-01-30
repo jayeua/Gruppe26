462d331c814db9c3916637dc91ff3f0da20aff2f
jayscott <ninjaspyjamas31@gmail.com>
2026-01-30 11:13:26 +0100

    update jira sprint 3 status

--- scripts/fetch_jira.js
+++ scripts/fetch_jira.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const JIRA_BASE_URL = process.env.JIRA_BASE_URL;
 const JIRA_EMAIL = process.env.JIRA_EMAIL;
